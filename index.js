179612face927845777bf9570c308b944a2ac22b
Cao Kiến Hựu <137107970+CaoKienHuu2004@users.noreply.github.com>
2023-06-24 12:07:12 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Tăng chiều rộng thanh điều hướng khi click*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+/* Giảm chiều rộng bằng 0 khi đóng thành điều hướng */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
